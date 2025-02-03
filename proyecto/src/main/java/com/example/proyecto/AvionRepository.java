6cbd56a354941448bd92eed37d76109802bd956c
Oskar1488 <145151178+Oskar1488@users.noreply.github.com>
2025-02-03 13:44:38 +0100

    Delete proyecto directory

--- proyecto/src/main/java/com/example/proyecto/AvionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.proyecto;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AvionRepository extends JpaRepository<Avion, Integer> {
-}
